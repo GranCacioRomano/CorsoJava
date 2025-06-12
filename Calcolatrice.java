af09eaf4d542c1edd525f628c9cee5e8625dbd13
Diego Musardo <diegomusardo.dm@gmail.com>
2025-06-12 12:22:21 +0200

    Ex2
    feat: aggiunta calcolatrice avanzata con gestione multipli calcoli e somma totale
    
    - Creata la classe CalcolatriceAvanzata.java che permette di eseguire N calcoli scelti dall'utente.
    - Ogni calcolo richiede due numeri e un'operazione tra +, -, *, /.
    - I risultati vengono salvati in un array e stampati al termine.
    - Aggiunta la somma totale dei risultati come funzionalità bonus.
    - Gestione sicura della divisione per zero e input dell'utente.

---
+++ Calcolatrice.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Calcolatrice {
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Quanti calcoli vuoi fare?");
+        int numeroCalcoli = scanner.nextInt();
+        
+        double[] risultati = new double[numeroCalcoli]; 
+        double sommaTotale = 0;
+
+        for (int i = 0; i < numeroCalcoli; i++) {
+            System.out.println("Inserisci il primo numero:");
+            double primoNumero = scanner.nextDouble();
+            
+            System.out.println("Inserisci il secondo numero:");
+            double secondoNumero = scanner.nextDouble();
+            
+            System.out.println("Scegli l'operazione: +, -, *, /");
+            String operazione = scanner.next();
+
+            
+            switch (operazione) {
+                case "+":
+                    risultati[i] = primoNumero + secondoNumero;
+                    break;
+                case "-":
+                    risultati[i] = primoNumero - secondoNumero;
+                    break;
+                case "*":
+                    risultati[i] = primoNumero * secondoNumero;
+                    break;
+                case "/":
+                    if (secondoNumero != 0) {
+                        risultati[i] = primoNumero / secondoNumero;
+                    } else {
+                        System.out.println("Errore: divisione per zero.");
+                        risultati[i] = 0; // Imposta a zero in caso di errore
+                    }
+                    break;
+                default:
+                    System.out.println("Operazione non valida.");
+                    risultati[i] = 0; // Imposta a zero in caso di operazione non valida
+            }
+            System.out.println("Risultato del calcolo " + (i + 1) + ": " + risultati[i]);
+            sommaTotale += risultati[i];
+        }
+        
+        System.out.println("Risultati dei calcoli:");
+        for (int i = 0; i < risultati.length; i++) {
+            System.out.println("Calcolo " + (i + 1) + ": " + risultati[i]);
+        }
+        
+        System.out.println("Somma totale dei risultati: " + sommaTotale);
+        
+        scanner.close();
+    }
+
+}
+
